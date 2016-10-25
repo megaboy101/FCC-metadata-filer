4962cc809517c290f15eb7246e36a43a6082187e
jacob bleser <jacob bleser>
2016-10-24 21:46:32 -0400

    Finished controller and views

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,15 @@
-'use strict';
-
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
 
-module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
+module.exports = function (app) {
+   var clickHandler = new ClickHandler();
 
    app.route('/')
       .get(function (req, res) {
          res.sendFile(process.cwd() + '/public/index.html');
       });
 
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
+   app.route('/api')
+      .post(upload.single('upl'), clickHandler.analyzeFile);
 };
--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 var express = require('express');
-var mongo = require('mongodb');
 var routes = require('./app/routes/index.js');
 
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-   if (err) {
-      throw new Error('Database failed to connect!');
-   } else {
-      console.log('Successfully connected to MongoDB on mLab.');
-   }
+routes(app);
 
-   app.use('/public', express.static(process.cwd() + '/public'));
-   app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-
-   routes(app, db);
-
-   app.listen(process.env.PORT || 3000, function () {
-      console.log('Node.js listening...');
-   });
-
+app.listen(process.env.PORT || 3000, function () {
+   console.log('Node.js listening...');
 });
